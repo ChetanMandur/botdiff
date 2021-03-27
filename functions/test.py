9212ce1026de1cfc4e919c34e017eadf7c633b1d
Chetan Mandur <ChetanMandur45@gmail.com>
2021-03-26 20:41:25 -0400

    Updated test.py with examples of Discord syntax

--- functions/test.py
+++ functions/test.py
@@ -1,2 +1,24 @@
-def test_command():
-    return "hello world!"
+def test_command(ctx, ext):
+    if ext == None:
+        return "hello world!"
+
+    if ext == "saymyname":
+        name = str(ctx.author).split("#")[0]
+        return f"{name}"
+
+    if ext == "bold":
+        return "**hello world!**"
+
+    if ext == "block":
+        return """> This is hopefully a block
+        > hello world!
+        > **ok goodbye**
+        > *just kidding!*
+        > ok fr goodbye"""
+
+    if ext == "colour":
+        return """ ```ini\n[please be blue]```
+this isnt blue :3"""
+
+    if ext == "emoji":
+        return ":scream: AHHHHHHHHHHH"
