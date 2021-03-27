2c975130659e447f64586bc782b7751db50cf9b8
Chetan Mandur <ChetanMandur45@gmail.com>
2021-03-26 21:48:47 -0400

    added basic riot watcher ability and provided test. Also added testing.py to .gitignore

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from riotwatcher import ApiError, LolWatcher
 
 from functions.test import test_command
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 # GUILD = os.getenv("DISCORD_GUILD")
+RIOT = os.getenv("RIOT_TOKEN")
 
+lol_watcher = LolWatcher(RIOT)
 bot = commands.Bot(command_prefix=">>")
 
 
 @bot.command()
 async def test(ctx, ext=None):
-    await ctx.send(test_command(ctx, ext))
+    await ctx.send(test_command(ctx, ext, lol_watcher))
 
 
 print("running!")
