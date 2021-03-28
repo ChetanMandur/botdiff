71ac31728c84b615920425982d5b280c163221ce
Chetan Mandur <ChetanMandur45@gmail.com>
2021-03-28 15:06:00 -0400

    updated some functions

--- functions/sum.py
+++ functions/sum.py
@@ -1,21 +1,26 @@
 def level(lol_watcher, ctx, sum_name, ext):
-    my_user = lol_watcher.summoner.by_name("na1", str(sum_name))
-    return f"This user is level {my_user['summonerLevel']}"
+    user = lol_watcher.summoner.by_name("na1", str(sum_name))
+    return f"This user is level {user['summonerLevel']}"
 
 
 def id(lol_watcher, ctx, sum_name, ext):
-    my_user = lol_watcher.summoner.by_name("na1", str(sum_name))
-    return f"""> **Username:** {my_user['name']}
-    > **ID:** {my_user['id']}
-    > **Account ID:** {my_user['accountId']}  """
+    user = lol_watcher.summoner.by_name("na1", str(sum_name))
+    return f"""> **Username:** {user['name']}
+    > **ID:** {user['id']}
+    > **Account ID:** {user['accountId']}  """
 
 
 def sum_command(lol_watcher, ctx, sum_name, ext):
     if sum_name == None:
-        return "Please provide a username"
+        return f"""> **Usage**
+        > `sum <UserName> <Command>` """
 
     if ext == None:
-        return "username only given"
+        user = lol_watcher.summoner.by_name("na1", str(sum_name))
+        return f"""> **Username:** {user['name']}
+        > **Level**: {user['summonerLevel']}
+        > **ID:** {user['id']}
+        > **Account ID:** {user['accountId']}  """
 
     else:
         switcher = {
