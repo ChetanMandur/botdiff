78d3548fa246c952a2c94e32c893252652c2baae
Chetan Mandur <ChetanMandur45@gmail.com>
2021-03-28 15:35:03 -0400

    Changed test.py to make use of dict for switching

--- functions/test.py
+++ functions/test.py
@@ -2,30 +2,47 @@ def test_command(ctx, ext, lol_watcher, sum_name):
     if ext == None:
         return "hello world!"
 
-    if ext == "saymyname":
-        name = str(ctx.author).split("#")[0]
-        return f"{name}"
+    else:
+        switcher = {
+            "saymyname": saymyname,
+            "bold": bold,
+            "block": block,
+            "colour": colour,
+            "emoji": emoji,
+            "lolsum": lolsum,
+        }
+        return switcher.get(ext, lambda: "Invalid arg")(ctx, ext, lol_watcher, sum_name)
 
-    if ext == "bold":
-        return "**hello world!**"
 
-    if ext == "block":
-        return """> This is hopefully a block
-        > hello world!
-        > **ok goodbye**
-        > *just kidding!*
-        > ok fr goodbye"""
+def saymyname(ctx, ext, lol_watcher, sum_name):
+    name = str(ctx.author).split("#")[0]
+    return f"{name}"
 
-    if ext == "colour":
-        return """ ```ini\n[please be blue]```
+
+def bold(ctx, ext, lol_watcher, sum_name):
+    return "**hello world!**"
+
+
+def block(ctx, ext, lol_watcher, sum_name):
+    return """> This is hopefully a block
+    > hello world!
+    > **ok goodbye**
+    > *just kidding!*
+    > ok fr goodbye"""
+
+
+def colour(ctx, ext, lol_watcher, sum_name):
+    return """ ```ini\n[please be blue]```
 this isnt blue :3"""
 
-    if ext == "emoji":
-        return ":scream: AHHHHHHHHHHH"
 
-    if ext == "lolsum":
-        if sum_name != None:
-            my_user = lol_watcher.summoner.by_name("na1", str(sum_name))
-            return f"{my_user['name']} is level {my_user['summonerLevel']} "
-        else:
-            return "provide a username to search :blush:"
+def emoji(ctx, ext, lol_watcher, sum_name):
+    return ":scream: AHHHHHHHHHHH"
+
+
+def lolsum(ctx, ext, lol_watcher, sum_name):
+    if sum_name != None:
+        my_user = lol_watcher.summoner.by_name("na1", str(sum_name))
+        return f"{my_user['name']} is level {my_user['summonerLevel']} "
+    else:
+        return "provide a username to search :blush:"
