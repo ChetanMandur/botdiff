de97f0a29a006f2579c42fb6de84796f6b967c32
Chetan Mandur <ChetanMandur45@gmail.com>
2021-04-02 16:46:47 -0400

    added error handling with the riot api

--- helpers/riot_helper.py
+++ helpers/riot_helper.py
@@ -1,6 +1,8 @@
 import os
 
+import discord
 from dotenv import load_dotenv
+from requests.exceptions import HTTPError
 from riotwatcher import ApiError, LolWatcher
 
 load_dotenv()
@@ -12,3 +14,14 @@ def queue_type_clean(queuetype):
     ranks = {"RANKED_SOLO_5x5": "Ranked Solo/Duo", "RANKED_TEAM_5x5": "Flex"}
 
     return ranks.get(queuetype)
+
+
+def error_handling(error):
+    if error.response.status_code == 403:
+        return "The Riot API token as expired. Please yell at admin."
+    elif error.response.status_code == 404:
+        return f"404 error... did you spell the name properly?"
+
+    else:
+        return f"""something broke... this is what I know:
+> {error}"""
