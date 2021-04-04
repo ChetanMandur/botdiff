66b7cafb728928e0a84b5f51bc98cb6354ffcb36
Jack Cui <jackcuikq@gmail.com>
2021-04-03 23:31:55 -0400

    added class RunePage

---
+++ data_structures/rune_page.py
@@ -0,0 +1,8 @@
+class RunePage:
+    def __init__(self, champ, role):
+        self.champ_searched = champ
+        self.role_searched = role
+        self.main_tree = []
+        self.sec_tree = []
+        self.shards_tree = []
+        self.type_names = []
