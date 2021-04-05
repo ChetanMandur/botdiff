bfc9c7c752336804fc82ae3cfea620e76d5945d6
Chetan Mandur <ChetanMandur45@gmail.com>
2021-04-05 16:58:23 -0400

    inital files

---
+++ data_structures/build_page.py
@@ -0,0 +1,8 @@
+class BuildPage:
+    def __init__(self, champ, role):
+        self.champ_searched = champ
+        self.role_searched = role
+        self.core_build = []
+        self.starter_items = []
+        self.boots = ""
+        self.type_names = []
